d390c336df883e0d6927f6a5def0b87ec7334a95
chawkk6404 <chawk_k@outlook.com>
2024-01-15 22:03:52 -0800

    Skills update

--- src/skills.cpp
+++ src/skills.cpp
@@ -5,45 +5,37 @@
 // Skills Auton
 ///
 void skills() {
-    // TODO: Run right flap
+    chassis.set_angle(135);
 
-    chassis.set_turn_pid(45, TURN_SPEED);
+    chassis.set_drive_pid(-5, DRIVE_SPEED);
     chassis.wait_drive();
 
-    chassis.set_drive_pid(6, DRIVE_SPEED);
-    chassis.wait_drive();
-
-    chassis.set_turn_pid(-20, TURN_SPEED);
-    chassis.wait_drive();
-
-    chassis.set_drive_pid(5, DRIVE_SPEED);
-    chassis.wait_drive();
-
-    catapult.move_velocity(catapultVelocity);
-    pros::delay(50000);
-    catapult.brake();
-
-    chassis.set_turn_pid(-80, TURN_SPEED);
+    chassis.set_swing_pid(ez::RIGHT_SWING, 180, SWING_SPEED);
     chassis.wait_drive();
 
     chassis.set_drive_pid(-10, DRIVE_SPEED, true);
     chassis.wait_drive();
 
-    chassis.set_drive_pid(5, DRIVE_SPEED);
+    chassis.set_swing_pid(ez::RIGHT_SWING, 75, SWING_SPEED);
     chassis.wait_drive();
 
     chassis.set_drive_pid(-5, DRIVE_SPEED);
     chassis.wait_drive();
 
-    chassis.set_swing_pid(ez::RIGHT_SWING, -100, SWING_SPEED);
+    catapult.move_velocity(catapultVelocity);
+    pros::delay(45);
+
+    chassis.set_swing_pid(ez::LEFT_SWING, 135, SWING_SPEED);
     chassis.wait_drive();
 
-    chassis.set_drive_pid(10, DRIVE_SPEED);
+    chassis.set_drive_pid(5, DRIVE_SPEED);
     chassis.wait_drive();
 
-    chassis.set_turn_pid(205, TURN_SPEED);
+    chassis.set_swing_pid(ez::RIGHT_SWING, 90, SWING_SPEED);
     chassis.wait_drive();
 
-    chassis.set_drive_pid(30, DRIVE_SPEED, true);
+    chassis.set_drive_pid(60, DRIVE_SPEED, true);
     chassis.wait_drive();
+
+
 }
